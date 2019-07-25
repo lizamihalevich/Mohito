aecf45613888695923d78c237bc07666ce1654fc
Liza Mihalevich <lizamih99@gmail.com>
2019-07-25 19:39:46 +0300

    webpack

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <div>
+            hey
+        </div>
+    );
+}
+
+ReactDOM.render(
+        <App />
+    window.document.getElementById('app'),
+);
